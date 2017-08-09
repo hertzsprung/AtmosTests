d32dbe4e6cb0994e7777628dfe7d2815d7596329
James Shaw <js102@zepler.net>
2017-08-09 15:14:29 +0100

    add mountains up to 6km high for resting test

--- generators/resting.py
+++ generators/resting.py
@@ -11,27 +11,39 @@ class Resting:
         meshBtf1000m = TerrainFollowingMesh('resting-mesh-btf-1000m', meshNoOrography, os.path.join('src/resting/mesh-btf-1000m'))
         meshBtf2000m = TerrainFollowingMesh('resting-mesh-btf-2000m', meshNoOrography, os.path.join('src/resting/mesh-btf-2000m'))
         meshBtf3000m = TerrainFollowingMesh('resting-mesh-btf-3000m', meshNoOrography, os.path.join('src/resting/mesh-btf-3000m'))
+        meshBtf4000m = TerrainFollowingMesh('resting-mesh-btf-4000m', meshNoOrography, os.path.join('src/resting/mesh-btf-4000m'))
+        meshBtf5000m = TerrainFollowingMesh('resting-mesh-btf-5000m', meshNoOrography, os.path.join('src/resting/mesh-btf-5000m'))
+        meshBtf6000m = TerrainFollowingMesh('resting-mesh-btf-6000m', meshNoOrography, os.path.join('src/resting/mesh-btf-6000m'))
 
         meshSleve1000m = TerrainFollowingMesh('resting-mesh-sleve-1000m', meshNoOrography, os.path.join('src/resting/mesh-sleve-1000m'))
         meshSleve2000m = TerrainFollowingMesh('resting-mesh-sleve-2000m', meshNoOrography, os.path.join('src/resting/mesh-sleve-2000m'))
         meshSleve3000m = TerrainFollowingMesh('resting-mesh-sleve-3000m', meshNoOrography, os.path.join('src/resting/mesh-sleve-3000m'))
+        meshSleve4000m = TerrainFollowingMesh('resting-mesh-sleve-4000m', meshNoOrography, os.path.join('src/resting/mesh-sleve-4000m'))
+        meshSleve5000m = TerrainFollowingMesh('resting-mesh-sleve-5000m', meshNoOrography, os.path.join('src/resting/mesh-sleve-5000m'))
+        meshSleve6000m = TerrainFollowingMesh('resting-mesh-sleve-6000m', meshNoOrography, os.path.join('src/resting/mesh-sleve-6000m'))
 
         createPatchDict = os.path.join('src/resting/createPatchDict')
 
         meshCutCell1000m = CutCellMesh('resting-mesh-cutCell-1000m', os.path.join('src/resting/mesh-cutCell-1000m'), createPatchDict)
         meshCutCell2000m = CutCellMesh('resting-mesh-cutCell-2000m', os.path.join('src/resting/mesh-cutCell-2000m'), createPatchDict)
         meshCutCell3000m = CutCellMesh('resting-mesh-cutCell-3000m', os.path.join('src/resting/mesh-cutCell-3000m'), createPatchDict)
+        meshCutCell4000m = CutCellMesh('resting-mesh-cutCell-4000m', os.path.join('src/resting/mesh-cutCell-4000m'), createPatchDict)
+        meshCutCell5000m = CutCellMesh('resting-mesh-cutCell-5000m', os.path.join('src/resting/mesh-cutCell-5000m'), createPatchDict)
+        meshCutCell6000m = CutCellMesh('resting-mesh-cutCell-6000m', os.path.join('src/resting/mesh-cutCell-6000m'), createPatchDict)
 
         meshSlantedCell1000m = SlantedCellMesh('resting-mesh-slantedCell-1000m', meshNoOrography, os.path.join('src/resting/mesh-slantedCell-1000m'))
         meshSlantedCell2000m = SlantedCellMesh('resting-mesh-slantedCell-2000m', meshNoOrography, os.path.join('src/resting/mesh-slantedCell-2000m'))
         meshSlantedCell3000m = SlantedCellMesh('resting-mesh-slantedCell-3000m', meshNoOrography, os.path.join('src/resting/mesh-slantedCell-3000m'))
+        meshSlantedCell4000m = SlantedCellMesh('resting-mesh-slantedCell-4000m', meshNoOrography, os.path.join('src/resting/mesh-slantedCell-4000m'))
+        meshSlantedCell5000m = SlantedCellMesh('resting-mesh-slantedCell-5000m', meshNoOrography, os.path.join('src/resting/mesh-slantedCell-5000m'))
+        meshSlantedCell6000m = SlantedCellMesh('resting-mesh-slantedCell-6000m', meshNoOrography, os.path.join('src/resting/mesh-slantedCell-6000m'))
 
         self.meshes = [
                 meshNoOrography,
-                meshBtf1000m, meshBtf2000m, meshBtf3000m,
-                meshSleve1000m, meshSleve2000m, meshSleve3000m,
-                meshCutCell1000m, meshCutCell2000m, meshCutCell3000m,
-                meshSlantedCell1000m, meshSlantedCell2000m, meshSlantedCell3000m
+                meshBtf1000m, meshBtf2000m, meshBtf3000m, meshBtf4000m, meshBtf5000m, meshBtf6000m,
+                meshSleve1000m, meshSleve2000m, meshSleve3000m, meshSleve4000m, meshSleve5000m, meshSleve6000m,
+                meshCutCell1000m, meshCutCell2000m, meshCutCell3000m, meshCutCell4000m, meshCutCell5000m, meshCutCell6000m,
+                meshSlantedCell1000m, meshSlantedCell2000m, meshSlantedCell3000m, meshSlantedCell4000m, meshSlantedCell5000m, meshSlantedCell6000m
         ]
 
         # maxwByMountainHeight
@@ -46,6 +58,9 @@ class Resting:
                     RestingCollated.Test('resting-btf-1000m-linearUpwind', 1000, meshBtf1000m, timestep=25),
                     RestingCollated.Test('resting-btf-2000m-linearUpwind', 2000, meshBtf2000m, timestep=25),
                     RestingCollated.Test('resting-btf-3000m-linearUpwind', 3000, meshBtf3000m, timestep=25),
+                    RestingCollated.Test('resting-btf-4000m-linearUpwind', 4000, meshBtf4000m, timestep=25),
+                    RestingCollated.Test('resting-btf-5000m-linearUpwind', 5000, meshBtf5000m, timestep=25),
+                    RestingCollated.Test('resting-btf-6000m-linearUpwind', 6000, meshBtf6000m, timestep=25)
         ])
 
         self.btfCubicFit = resting.collateByMountainHeight(
@@ -56,6 +71,9 @@ class Resting:
                     RestingCollated.Test('resting-btf-1000m-cubicFit', 1000, meshBtf1000m, timestep=25),
                     RestingCollated.Test('resting-btf-2000m-cubicFit', 2000, meshBtf2000m, timestep=25),
                     RestingCollated.Test('resting-btf-3000m-cubicFit', 3000, meshBtf3000m, timestep=25),
+                    RestingCollated.Test('resting-btf-4000m-cubicFit', 4000, meshBtf4000m, timestep=25),
+                    RestingCollated.Test('resting-btf-5000m-cubicFit', 5000, meshBtf5000m, timestep=25),
+                    RestingCollated.Test('resting-btf-6000m-cubicFit', 6000, meshBtf6000m, timestep=25)
         ])
 
         self.sleveLinearUpwind = resting.collateByMountainHeight(
@@ -66,6 +84,9 @@ class Resting:
                     RestingCollated.Test('resting-sleve-1000m-linearUpwind', 1000, meshSleve1000m, timestep=25),
                     RestingCollated.Test('resting-sleve-2000m-linearUpwind', 2000, meshSleve2000m, timestep=25),
                     RestingCollated.Test('resting-sleve-3000m-linearUpwind', 3000, meshSleve3000m, timestep=25),
+                    RestingCollated.Test('resting-sleve-4000m-linearUpwind', 4000, meshSleve4000m, timestep=25),
+                    RestingCollated.Test('resting-sleve-5000m-linearUpwind', 5000, meshSleve5000m, timestep=25),
+                    RestingCollated.Test('resting-sleve-6000m-linearUpwind', 6000, meshSleve6000m, timestep=25)
         ])
 
         self.sleveCubicFit = resting.collateByMountainHeight(
@@ -76,6 +97,9 @@ class Resting:
                     RestingCollated.Test('resting-sleve-1000m-cubicFit', 1000, meshSleve1000m, timestep=25),
                     RestingCollated.Test('resting-sleve-2000m-cubicFit', 2000, meshSleve2000m, timestep=25),
                     RestingCollated.Test('resting-sleve-3000m-cubicFit', 3000, meshSleve3000m, timestep=25),
+                    RestingCollated.Test('resting-sleve-4000m-cubicFit', 4000, meshSleve4000m, timestep=25),
+                    RestingCollated.Test('resting-sleve-5000m-cubicFit', 5000, meshSleve5000m, timestep=25),
+                    RestingCollated.Test('resting-sleve-6000m-cubicFit', 6000, meshSleve6000m, timestep=25)
         ])
 
         self.cutCellLinearUpwind = resting.collateByMountainHeight(
@@ -86,6 +110,9 @@ class Resting:
                     RestingCollated.Test('resting-cutCell-1000m-linearUpwind', 1000, meshCutCell1000m, timestep=25),
                     RestingCollated.Test('resting-cutCell-2000m-linearUpwind', 2000, meshCutCell2000m, timestep=25),
                     RestingCollated.Test('resting-cutCell-3000m-linearUpwind', 3000, meshCutCell3000m, timestep=25),
+                    RestingCollated.Test('resting-cutCell-4000m-linearUpwind', 4000, meshCutCell4000m, timestep=25),
+                    RestingCollated.Test('resting-cutCell-5000m-linearUpwind', 5000, meshCutCell5000m, timestep=25),
+                    RestingCollated.Test('resting-cutCell-6000m-linearUpwind', 6000, meshCutCell6000m, timestep=25)
         ])
 
         self.cutCellCubicFit = resting.collateByMountainHeight(
@@ -96,6 +123,9 @@ class Resting:
                     RestingCollated.Test('resting-cutCell-1000m-cubicFit', 1000, meshCutCell1000m, timestep=25),
                     RestingCollated.Test('resting-cutCell-2000m-cubicFit', 2000, meshCutCell2000m, timestep=25),
                     RestingCollated.Test('resting-cutCell-3000m-cubicFit', 3000, meshCutCell3000m, timestep=25),
+                    RestingCollated.Test('resting-cutCell-4000m-cubicFit', 4000, meshCutCell4000m, timestep=25),
+                    RestingCollated.Test('resting-cutCell-5000m-cubicFit', 5000, meshCutCell5000m, timestep=25),
+                    RestingCollated.Test('resting-cutCell-6000m-cubicFit', 6000, meshCutCell6000m, timestep=25)
         ])
 
         self.slantedCellLinearUpwind = resting.collateByMountainHeight(
@@ -106,6 +136,9 @@ class Resting:
                     RestingCollated.Test('resting-slantedCell-1000m-linearUpwind', 1000, meshSlantedCell1000m, timestep=25),
                     RestingCollated.Test('resting-slantedCell-2000m-linearUpwind', 2000, meshSlantedCell2000m, timestep=25),
                     RestingCollated.Test('resting-slantedCell-3000m-linearUpwind', 3000, meshSlantedCell3000m, timestep=25),
+                    RestingCollated.Test('resting-slantedCell-4000m-linearUpwind', 4000, meshSlantedCell4000m, timestep=25),
+                    RestingCollated.Test('resting-slantedCell-5000m-linearUpwind', 5000, meshSlantedCell5000m, timestep=25),
+                    RestingCollated.Test('resting-slantedCell-6000m-linearUpwind', 6000, meshSlantedCell6000m, timestep=25)
         ])
 
         self.slantedCellCubicFit = resting.collateByMountainHeight(
@@ -116,6 +149,9 @@ class Resting:
                     RestingCollated.Test('resting-slantedCell-1000m-cubicFit', 1000, meshSlantedCell1000m, timestep=25),
                     RestingCollated.Test('resting-slantedCell-2000m-cubicFit', 2000, meshSlantedCell2000m, timestep=25),
                     RestingCollated.Test('resting-slantedCell-3000m-cubicFit', 3000, meshSlantedCell3000m, timestep=25),
+                    RestingCollated.Test('resting-slantedCell-4000m-cubicFit', 4000, meshSlantedCell4000m, timestep=25),
+                    RestingCollated.Test('resting-slantedCell-5000m-cubicFit', 5000, meshSlantedCell5000m, timestep=25),
+                    RestingCollated.Test('resting-slantedCell-6000m-cubicFit', 6000, meshSlantedCell6000m, timestep=25)
         ])
 
     def addTo(self, build):
