cfe53d714470bcf76962d0c30e219b8c040fa65d
James Shaw <js102@zepler.net>
2017-08-14 12:27:13 +0000

    add upload script

---
+++ upload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+./singularity.ninja.sh $@ build/{schaer,tf}Advect-{btf,cutCell}-1000-{linearUpwind,cubicFit}/10000/{l2,linf}errorT.txt
+./singularity.ninja.sh $@ build/schaerAdvect-{btf,cutCell}-1000-linearUpwind/dt.txt
+./singularity.ninja.sh $@ build/{schaer,tf}Advect-{btf,cutCell}-1000-{linearUpwind,cubicFit}/s3.uploaded
+./singularity.ninja.sh $@ build/schaerAdvect-{btf,cutCell}-{linearUpwind,cubicFit}-collated/s3.uploaded
