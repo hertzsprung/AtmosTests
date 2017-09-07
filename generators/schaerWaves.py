98c42e23baa19d4dbd01eab4b190a1d6f14ef0ef
James Shaw <js102@zepler.net>
2017-09-07 16:14:55 +0100

    add fastMesh for schaerWaves

--- generators/schaerWaves.py
+++ generators/schaerWaves.py
@@ -6,16 +6,19 @@ import os
 
 class SchaerWaves:
     def __init__(self, parallel, fast):
+        meshNoOrographyFast = BlockMesh('schaerWaves-mesh-noOrography-fast', os.path.join('src/schaerWaves/mesh-noOrography-fast'))
         meshNoOrography300dz = BlockMesh('schaerWaves-mesh-noOrography-300dz', os.path.join('src/schaerWaves/mesh-noOrography-300dz'))
 
-        meshBtf300dz = TerrainFollowingMesh('schaerWaves-mesh-btf-300dz', meshNoOrography300dz, os.path.join('src/schaerWaves/mesh-btf'))
+        meshBtfDict = os.path.join('src/schaerWaves/mesh-btf')
+        meshBtfFast = TerrainFollowingMesh('schaerWaves-mesh-btf-fast', meshNoOrographyFast, meshBtfDict)
+        meshBtf300dz = TerrainFollowingMesh('schaerWaves-mesh-btf-300dz', meshNoOrography300dz, meshBtfDict)
 
         self.meshes = [
-                meshNoOrography300dz,
-                meshBtf300dz
+                meshNoOrographyFast, meshNoOrography300dz,
+                meshBtfFast, meshBtf300dz
         ]
 
-        self.btfLinearUpwind300dz = ninja.SchaerWaves('schaerWaves-btf-300dz-linearUpwind', meshBtf300dz, 8.0, os.path.join('src/schaerWaves/linearUpwind'), parallel, fast)
+        self.btfLinearUpwind300dz = ninja.SchaerWaves('schaerWaves-btf-300dz-linearUpwind', meshBtf300dz, 8.0, os.path.join('src/schaerWaves/linearUpwind'), parallel, fast, meshBtfFast)
 
     def addTo(self, build):
         build.addAll(self.meshes)
