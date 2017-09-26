118772e8efc84761fec11cbacb03cb81691a0892
James Shaw <js102@zepler.net>
2017-09-26 09:45:50 +0100

    create thermalAdvect 0/T

--- generators/thermalAdvect.py
+++ generators/thermalAdvect.py
@@ -1,6 +1,14 @@
+from ninjaopenfoam import Advect
+
+import os
+
 class ThermalAdvect:
     def __init__(self, meshes, parallel, fast):
-        pass
+        tracerField = os.path.join('src/thermalAdvect/tracerField')
+        velocityField = os.path.join('src/thermalAdvect/velocityField')
+        linearUpwind = os.path.join('src/schaerAdvect/linearUpwind')
+
+        self.btf300dzLinearUpwind = Advect('thermalAdvect-btf-300dz-linearUpwind', 0, 250, meshes.btf300dz, tracerField, velocityField, 8, linearUpwind, parallel, fast)
 
     def addTo(self, build):
-        pass
+        build.add(self.btf300dzLinearUpwind)
