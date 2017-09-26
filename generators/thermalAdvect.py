01093ba75336678eed380afdedcf23800fac81d9
James Shaw <js102@zepler.net>
2017-09-26 13:58:35 +0100

    specify Timing for thermalAdvect

--- generators/thermalAdvect.py
+++ generators/thermalAdvect.py
@@ -1,4 +1,4 @@
-from ninjaopenfoam import Advect
+from ninjaopenfoam import Advect, Timing
 
 import os
 
@@ -8,7 +8,7 @@ class ThermalAdvect:
         velocityField = os.path.join('src/thermalAdvect/velocityField')
         linearUpwind = os.path.join('src/schaerAdvect/linearUpwind')
 
-        self.btf300dzLinearUpwind = Advect('thermalAdvect-btf-300dz-linearUpwind', 0, 250, meshes.btf300dz, tracerField, velocityField, 8, linearUpwind, parallel, fast)
+        self.btf300dzLinearUpwind = Advect('thermalAdvect-btf-300dz-linearUpwind', 0, 250, meshes.btf300dz, tracerField, velocityField, Timing(18000, 3600, 8), linearUpwind, parallel, fast)
 
     def addTo(self, build):
         build.add(self.btf300dzLinearUpwind)
