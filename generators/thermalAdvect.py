1c3aaae6d3c402d71eab79923dd57f460aeaa9dc
James Shaw <js102@zepler.net>
2017-09-28 10:25:40 +0100

    specify T_init as theta_init in thermalAdvect

--- generators/thermalAdvect.py
+++ generators/thermalAdvect.py
@@ -6,9 +6,13 @@ class ThermalAdvect:
     def __init__(self, meshes, parallel, fast):
         tracerField = os.path.join('src/thermalAdvect/tracerField')
         velocityField = os.path.join('src/thermalAdvect/velocityField')
+        linearUpwind = os.path.join('src/schaerAdvect/linearUpwind')
         cubicFit = os.path.join('src/schaerAdvect/cubicFit')
+        T_init = os.path.join('src/schaerWaves/theta_init')
 
-        self.btf300dzCubicFit = Advect('thermalAdvect-btf-300dz-cubicFit', 0, 250, meshes.btf300dz, tracerField, velocityField, Timing(18000, 3600, 8), cubicFit, parallel, fast)
+        self.btf300dzLinearUpwind = Advect('thermalAdvect-btf-300dz-linearUpwind', 0, 250, meshes.btf300dz, tracerField, velocityField, T_init, Timing(18000, 3600, 8), linearUpwind, parallel, fast)
+        self.btf300dzCubicFit = Advect('thermalAdvect-btf-300dz-cubicFit', 0, 250, meshes.btf300dz, tracerField, velocityField, T_init, Timing(18000, 3600, 8), cubicFit, parallel, fast)
 
     def addTo(self, build):
+        build.add(self.btf300dzLinearUpwind)
         build.add(self.btf300dzCubicFit)
