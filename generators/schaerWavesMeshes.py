6473433484afc7d88ab5d007aea0cc90afb57a17
James Shaw <js102@zepler.net>
2017-09-28 14:40:30 +0100

    add thermalAdvect btf-150dz

--- generators/schaerWavesMeshes.py
+++ generators/schaerWavesMeshes.py
@@ -4,18 +4,22 @@ import os
 
 class SchaerWavesMeshes:
     def __init__(self):
+        # 500, 300, 250, 200, 150, 125, 100, 75, 50
+
         noOrographyFast = BlockMesh('schaerWaves-mesh-noOrography-fast', os.path.join('src/schaerWaves/mesh-noOrography-fast'))
         noOrography300dz = BlockMesh('schaerWaves-mesh-noOrography-300dz', os.path.join('src/schaerWaves/mesh-noOrography-300dz'))
+        noOrography150dz = BlockMesh('schaerWaves-mesh-noOrography-150dz', os.path.join('src/schaerWaves/mesh-noOrography-150dz'))
         noOrography50dz = BlockMesh('schaerWaves-mesh-noOrography-50dz', os.path.join('src/schaerWaves/mesh-noOrography-50dz'))
 
         btfDict = os.path.join('src/schaerWaves/mesh-btf')
         self.btfFast = TerrainFollowingMesh('schaerWaves-mesh-btf-fast', noOrographyFast, btfDict)
         self.btf300dz = TerrainFollowingMesh('schaerWaves-mesh-btf-300dz', noOrography300dz, btfDict)
+        self.btf150dz = TerrainFollowingMesh('schaerWaves-mesh-btf-150dz', noOrography150dz, btfDict)
         self.btf50dz = TerrainFollowingMesh('schaerWaves-mesh-btf-50dz', noOrography50dz, btfDict)
 
         self.meshes = [
-                noOrographyFast, noOrography300dz, noOrography50dz,
-                self.btfFast, self.btf300dz, self.btf50dz
+                noOrographyFast, noOrography300dz, noOrography150dz, noOrography50dz,
+                self.btfFast, self.btf300dz, self.btf150dz, self.btf50dz
         ]
 
     def addTo(self, build):
--- generators/thermalAdvect.py
+++ generators/thermalAdvect.py
@@ -13,6 +13,9 @@ class ThermalAdvect:
         self.btf300dzLinearUpwind = Advect('thermalAdvect-btf-300dz-linearUpwind', 0, 250, meshes.btf300dz, tracerField, velocityField, T_init, Timing(18000, 3600, 8), linearUpwind, parallel, fast)
         self.btf300dzCubicFit = Advect('thermalAdvect-btf-300dz-cubicFit', 0, 250, meshes.btf300dz, tracerField, velocityField, T_init, Timing(18000, 3600, 8), cubicFit, parallel, fast)
 
+        self.btf150dzLinearUpwind = Advect('thermalAdvect-btf-150dz-linearUpwind', 0, 125, meshes.btf150dz, tracerField, velocityField, T_init, Timing(18000, 3600, 8), linearUpwind, parallel, fast)
+
     def addTo(self, build):
         build.add(self.btf300dzLinearUpwind)
         build.add(self.btf300dzCubicFit)
+        build.add(self.btf150dzLinearUpwind)
