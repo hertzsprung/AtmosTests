52bb28057d22387605645f449f11d9d433b13f04
James Shaw <js102@zepler.net>
2017-09-28 09:02:11 +0100

    use cubicFit for thermalAdvect, not linearUpwind

--- generators/thermalAdvect.py
+++ generators/thermalAdvect.py
@@ -6,9 +6,9 @@ class ThermalAdvect:
     def __init__(self, meshes, parallel, fast):
         tracerField = os.path.join('src/thermalAdvect/tracerField')
         velocityField = os.path.join('src/thermalAdvect/velocityField')
-        linearUpwind = os.path.join('src/schaerAdvect/linearUpwind')
+        cubicFit = os.path.join('src/schaerAdvect/cubicFit')
 
-        self.btf300dzLinearUpwind = Advect('thermalAdvect-btf-300dz-linearUpwind', 0, 250, meshes.btf300dz, tracerField, velocityField, Timing(18000, 3600, 8), linearUpwind, parallel, fast)
+        self.btf300dzCubicFit = Advect('thermalAdvect-btf-300dz-cubicFit', 0, 250, meshes.btf300dz, tracerField, velocityField, Timing(18000, 3600, 8), cubicFit, parallel, fast)
 
     def addTo(self, build):
-        build.add(self.btf300dzLinearUpwind)
+        build.add(self.btf300dzCubicFit)
