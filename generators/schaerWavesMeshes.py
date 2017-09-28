bfdcd39b4aff79bee1ed515e57bb2deb19ad2880
James Shaw <js102@zepler.net>
2017-09-28 17:29:49 +0100

    add more schaerWaves/thermalAdvect meshes

--- generators/schaerWavesMeshes.py
+++ generators/schaerWavesMeshes.py
@@ -4,22 +4,34 @@ import os
 
 class SchaerWavesMeshes:
     def __init__(self):
-        # 500, 300, 250, 200, 150, 125, 100, 75, 50
-
         noOrographyFast = BlockMesh('schaerWaves-mesh-noOrography-fast', os.path.join('src/schaerWaves/mesh-noOrography-fast'))
+        noOrography500dz = BlockMesh('schaerWaves-mesh-noOrography-500dz', os.path.join('src/schaerWaves/mesh-noOrography-500dz'))
         noOrography300dz = BlockMesh('schaerWaves-mesh-noOrography-300dz', os.path.join('src/schaerWaves/mesh-noOrography-300dz'))
+        noOrography250dz = BlockMesh('schaerWaves-mesh-noOrography-250dz', os.path.join('src/schaerWaves/mesh-noOrography-250dz'))
+        noOrography200dz = BlockMesh('schaerWaves-mesh-noOrography-200dz', os.path.join('src/schaerWaves/mesh-noOrography-200dz'))
         noOrography150dz = BlockMesh('schaerWaves-mesh-noOrography-150dz', os.path.join('src/schaerWaves/mesh-noOrography-150dz'))
+        noOrography125dz = BlockMesh('schaerWaves-mesh-noOrography-125dz', os.path.join('src/schaerWaves/mesh-noOrography-125dz'))
+        noOrography100dz = BlockMesh('schaerWaves-mesh-noOrography-100dz', os.path.join('src/schaerWaves/mesh-noOrography-100dz'))
+        noOrography75dz = BlockMesh('schaerWaves-mesh-noOrography-75dz', os.path.join('src/schaerWaves/mesh-noOrography-75dz'))
         noOrography50dz = BlockMesh('schaerWaves-mesh-noOrography-50dz', os.path.join('src/schaerWaves/mesh-noOrography-50dz'))
 
         btfDict = os.path.join('src/schaerWaves/mesh-btf')
         self.btfFast = TerrainFollowingMesh('schaerWaves-mesh-btf-fast', noOrographyFast, btfDict)
+        self.btf500dz = TerrainFollowingMesh('schaerWaves-mesh-btf-500dz', noOrography500dz, btfDict)
         self.btf300dz = TerrainFollowingMesh('schaerWaves-mesh-btf-300dz', noOrography300dz, btfDict)
+        self.btf250dz = TerrainFollowingMesh('schaerWaves-mesh-btf-250dz', noOrography250dz, btfDict)
+        self.btf200dz = TerrainFollowingMesh('schaerWaves-mesh-btf-200dz', noOrography200dz, btfDict)
         self.btf150dz = TerrainFollowingMesh('schaerWaves-mesh-btf-150dz', noOrography150dz, btfDict)
+        self.btf125dz = TerrainFollowingMesh('schaerWaves-mesh-btf-125dz', noOrography125dz, btfDict)
+        self.btf100dz = TerrainFollowingMesh('schaerWaves-mesh-btf-100dz', noOrography100dz, btfDict)
+        self.btf75dz = TerrainFollowingMesh('schaerWaves-mesh-btf-75dz', noOrography75dz, btfDict)
         self.btf50dz = TerrainFollowingMesh('schaerWaves-mesh-btf-50dz', noOrography50dz, btfDict)
 
         self.meshes = [
-                noOrographyFast, noOrography300dz, noOrography150dz, noOrography50dz,
-                self.btfFast, self.btf300dz, self.btf150dz, self.btf50dz
+                noOrographyFast, noOrography500dz, noOrography300dz, noOrography250dz, noOrography200dz,
+                noOrography150dz, noOrography125dz, noOrography100dz, noOrography75dz, noOrography50dz,
+                self.btfFast, self.btf500dz, self.btf300dz, self.btf250dz, self.btf200dz, self.btf150dz, 
+                self.btf125dz, self.btf100dz, self.btf75dz, self.btf50dz
         ]
 
     def addTo(self, build):
--- generators/thermalAdvect.py
+++ generators/thermalAdvect.py
@@ -6,11 +6,14 @@ class ThermalAdvect:
     def __init__(self, meshes, parallel, fast):
         tracerField = os.path.join('src/thermalAdvect/tracerField')
         velocityField = os.path.join('src/thermalAdvect/velocityField')
-        linearUpwind = os.path.join('src/schaerAdvect/linearUpwind')
         cubicFit = os.path.join('src/schaerAdvect/cubicFit')
         T_init = os.path.join('src/schaerWaves/theta_init')
 
-        self.btf300dzLinearUpwind = ninja.ThermalAdvect('thermalAdvect-btf-300dz-linearUpwind', 0, meshes.btf300dz, 8, linearUpwind, parallel, fast, meshes.btfFast)
+        self.btf500dzCubicFit = ninja.ThermalAdvect('thermalAdvect-btf-500dz-cubicFit', 833.33333, meshes.btf500dz, 13.33333333, cubicFit, parallel, fast, meshes.btfFast)
+        self.btf300dzCubicFit = ninja.ThermalAdvect('thermalAdvect-btf-300dz-cubicFit', 500, meshes.btf300dz, 8.0, cubicFit, parallel, fast, meshes.btfFast)
+        self.btf250dzCubicFit = ninja.ThermalAdvect('thermalAdvect-btf-250dz-cubicFit', 416.6667, meshes.btf250dz, 6.66666666, cubicFit, parallel, fast, meshes.btfFast)
+        self.btf150dzCubicFit = ninja.ThermalAdvect('thermalAdvect-btf-150dz-cubicFit', 250, meshes.btf150dz, 4.0, cubicFit, parallel, fast, meshes.btfFast)
+        self.btf75dzCubicFit = ninja.ThermalAdvect('thermalAdvect-btf-75dz-cubicFit', 125, meshes.btf75dz, 2.0, cubicFit, parallel, fast, meshes.btfFast)
 
     def addTo(self, build):
-        build.add(self.btf300dzLinearUpwind)
+        build.add(self.btf300dzCubicFit)
