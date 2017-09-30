9d7fb6c03283ff74637851fdc488d161adcabec3
James Shaw <js102@zepler.net>
2017-09-30 08:30:08 +0100

    up/download thermalAdvect tests

--- download.sh
+++ download.sh
@@ -8,23 +8,26 @@ aws s3 cp --recursive ${s3uri}schaerAdvect-btf-1000-linearUpwind build/schaerAdv
 aws s3 cp --recursive ${s3uri}schaerAdvect-cutCell-1000-linearUpwind build/schaerAdvect-cutCell-1000-linearUpwind
 aws s3 cp --recursive ${s3uri}schaerAdvect-btf-1000-cubicFit build/schaerAdvect-btf-1000-cubicFit
 aws s3 cp --recursive ${s3uri}schaerAdvect-cutCell-1000-cubicFit build/schaerAdvect-cutCell-1000-cubicFit
-aws s3 cp --recursive ${s3uri}tfAdvect-btf-1000-linearUpwind build/tfAdvect-btf-1000-linearUpwind
-aws s3 cp --recursive ${s3uri}tfAdvect-cutCell-1000-linearUpwind build/tfAdvect-cutCell-1000-linearUpwind
-aws s3 cp --recursive ${s3uri}tfAdvect-btf-1000-cubicFit build/tfAdvect-btf-1000-cubicFit
-aws s3 cp --recursive ${s3uri}tfAdvect-cutCell-1000-cubicFit build/tfAdvect-cutCell-1000-cubicFit
 aws s3 cp --recursive ${s3uri}schaerAdvect-btf-linearUpwind-collated build/schaerAdvect-btf-linearUpwind-collated
 aws s3 cp --recursive ${s3uri}schaerAdvect-cutCell-linearUpwind-collated build/schaerAdvect-cutCell-linearUpwind-collated
 aws s3 cp --recursive ${s3uri}schaerAdvect-btf-cubicFit-collated build/schaerAdvect-btf-cubicFit-collated
 aws s3 cp --recursive ${s3uri}schaerAdvect-cutCell-cubicFit-collated build/schaerAdvect-cutCell-cubicFit-collated
+
+aws s3 cp --recursive ${s3uri}tfAdvect-btf-1000-linearUpwind build/tfAdvect-btf-1000-linearUpwind
+aws s3 cp --recursive ${s3uri}tfAdvect-cutCell-1000-linearUpwind build/tfAdvect-cutCell-1000-linearUpwind
+aws s3 cp --recursive ${s3uri}tfAdvect-btf-1000-cubicFit build/tfAdvect-btf-1000-cubicFit
+aws s3 cp --recursive ${s3uri}tfAdvect-cutCell-1000-cubicFit build/tfAdvect-cutCell-1000-cubicFit
 aws s3 cp --recursive ${s3uri}tfAdvect-btf-linearUpwind-collated build/tfAdvect-btf-linearUpwind-collated
 aws s3 cp --recursive ${s3uri}tfAdvect-cutCell-linearUpwind-collated build/tfAdvect-cutCell-linearUpwind-collated
 aws s3 cp --recursive ${s3uri}tfAdvect-btf-cubicFit-collated build/tfAdvect-btf-cubicFit-collated
 aws s3 cp --recursive ${s3uri}tfAdvect-cutCell-cubicFit-collated build/tfAdvect-cutCell-cubicFit-collated
+
 aws s3 cp --recursive ${s3uri}deformationSphere-gaussians-hex-8-cubicFit build/deformationSphere-gaussians-hex-8-cubicFit
 aws s3 cp --recursive ${s3uri}deformationSphere-gaussians-hex-linearUpwind-collated build/deformationSphere-gaussians-hex-linearUpwind-collated
 aws s3 cp --recursive ${s3uri}deformationSphere-gaussians-hex-cubicFit-collated build/deformationSphere-gaussians-hex-cubicFit-collated
 aws s3 cp --recursive ${s3uri}deformationSphere-gaussians-cubedSphere-linearUpwind-collated build/deformationSphere-gaussians-cubedSphere-linearUpwind-collated
 aws s3 cp --recursive ${s3uri}deformationSphere-gaussians-cubedSphere-cubicFit-collated build/deformationSphere-gaussians-cubedSphere-cubicFit-collated
+
 aws s3 cp --recursive ${s3uri}mountainAdvect-h0-btf-1000-linearUpwind-collated build/mountainAdvect-h0-btf-1000-linearUpwind-collated
 aws s3 cp --recursive ${s3uri}mountainAdvect-h0-btf-1000-cubicFit-collated build/mountainAdvect-h0-btf-1000-cubicFit-collated
 aws s3 cp --recursive ${s3uri}mountainAdvect-h0-cutCell-1000-linearUpwind-collated build/mountainAdvect-h0-cutCell-1000-linearUpwind-collated
@@ -52,6 +55,7 @@ aws s3 cp --recursive ${s3uri}mountainAdvect-h0-slantedCell-1000-6000m-linearUpw
 aws s3 cp --recursive ${s3uri}mountainAdvect-maxdt-btf-6000m-cubicFit-collated build/mountainAdvect-maxdt-btf-6000m-cubicFit-collated
 aws s3 cp --recursive ${s3uri}mountainAdvect-maxdt-cutCell-6000m-cubicFit-collated build/mountainAdvect-maxdt-cutCell-6000m-cubicFit-collated
 aws s3 cp --recursive ${s3uri}mountainAdvect-maxdt-slantedCell-6000m-cubicFit-collated build/mountainAdvect-maxdt-slantedCell-6000m-cubicFit-collated
+
 aws s3 cp --recursive ${s3uri}resting-btf-linearUpwind-collated build/resting-btf-linearUpwind-collated
 aws s3 cp --recursive ${s3uri}resting-btf-cubicFit-collated build/resting-btf-cubicFit-collated
 aws s3 cp --recursive ${s3uri}resting-sleve-linearUpwind-collated build/resting-sleve-linearUpwind-collated
@@ -60,3 +64,31 @@ aws s3 cp --recursive ${s3uri}resting-sleve-cubicFit-collated build/resting-slev
 #aws s3 cp --recursive ${s3uri}resting-cutCell-cubicFit-collated build/resting-cutCell-cubicFit-collated
 aws s3 cp --recursive ${s3uri}resting-slantedCell-linearUpwind-collated build/resting-slantedCell-linearUpwind-collated
 aws s3 cp --recursive ${s3uri}resting-slantedCell-cubicFit-collated build/resting-slantedCell-cubicFit-collated
+
+aws s3 cp --recursive ${s3uri}thermalAdvect-btf-500dz-cubicFit build/thermalAdvect-btf-500dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-btf-300dz-cubicFit build/thermalAdvect-btf-300dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-btf-250dz-cubicFit build/thermalAdvect-btf-250dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-btf-200dz-cubicFit build/thermalAdvect-btf-200dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-btf-150dz-cubicFit build/thermalAdvect-btf-150dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-btf-125dz-cubicFit build/thermalAdvect-btf-125dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-btf-100dz-cubicFit build/thermalAdvect-btf-100dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-btf-75dz-cubicFit build/thermalAdvect-btf-75dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-btf-50dz-cubicFit build/thermalAdvect-btf-50dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-cutCell-500dz-cubicFit build/thermalAdvect-cutCell-500dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-cutCell-300dz-cubicFit build/thermalAdvect-cutCell-300dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-cutCell-250dz-cubicFit build/thermalAdvect-cutCell-250dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-cutCell-200dz-cubicFit build/thermalAdvect-cutCell-200dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-cutCell-150dz-cubicFit build/thermalAdvect-cutCell-150dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-cutCell-125dz-cubicFit build/thermalAdvect-cutCell-125dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-cutCell-100dz-cubicFit build/thermalAdvect-cutCell-100dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-cutCell-75dz-cubicFit build/thermalAdvect-cutCell-75dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-cutCell-50dz-cubicFit build/thermalAdvect-cutCell-50dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-slantedCell-500dz-cubicFit build/thermalAdvect-slantedCell-500dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-slantedCell-300dz-cubicFit build/thermalAdvect-slantedCell-300dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-slantedCell-250dz-cubicFit build/thermalAdvect-slantedCell-250dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-slantedCell-200dz-cubicFit build/thermalAdvect-slantedCell-200dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-slantedCell-150dz-cubicFit build/thermalAdvect-slantedCell-150dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-slantedCell-125dz-cubicFit build/thermalAdvect-slantedCell-125dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-slantedCell-100dz-cubicFit build/thermalAdvect-slantedCell-100dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-slantedCell-75dz-cubicFit build/thermalAdvect-slantedCell-75dz-cubicFit
+aws s3 cp --recursive ${s3uri}thermalAdvect-slantedCell-50dz-cubicFit build/thermalAdvect-slantedCell-50dz-cubicFit
--- upload.sh
+++ upload.sh
@@ -21,3 +21,6 @@ set -e
 ./singularity.ninja.sh $@ build/mountainAdvect-maxdt-{btf,cutCell,slantedCell}-6000m-cubicFit-collated/s3.uploaded
 
 ./singularity.ninja.sh $@ build/schaerWaves-btf-300dz-linearUpwind/s3.uploaded
+
+./singularity.ninja.sh $@ build/thermalAdvect-{btf,cutCell,slantedCell}-{500,300,250,200,150,125,100,75,50}dz-cubicFit/18000/T_diff.sampleLine.dat
+./singularity.ninja.sh $@ build/thermalAdvect-{btf,cutCell,slantedCell}-{500,300,250,200,150,125,100,75,50}dz-cubicFit/s3.uploaded
