2273a89f176c8f54502f31bdc1772aa7e12188a6
James Shaw <js102@zepler.net>
2017-10-04 21:45:29 +0100

    plot schaerWaves-cubicFit w contours, too

--- run.sh
+++ run.sh
@@ -21,7 +21,7 @@ ninja $@ build/mountainAdvect-h0-{btf,cutCell,slantedCell}-1000-5000m-linearUpwi
 ninja $@ build/resting-{btf,sleve,cutCell,slantedCell}-1000m-cubicFit/energy.dat
 ninja $@ build/resting-{btf,sleve,cutCell,slantedCell}-{linearUpwind,cubicFit}-collated/maxw.txt
 
-ninja $@ build/schaerWaves-btf-300dz-linearUpwind/18000/Uf
+ninja $@ build/schaerWaves-btf-300dz-{linearUpwind,cubicFit}/18000/Uf
 ninja $@ build/schaerWaves-{btf,cutCell,slantedCell}-{500,300,250,200,150,125,100,75,50}dz-cubicFit/18000/theta_diff.sampleLine.dat
 ninja $@ build/schaerWaves-btf-50dz-cubicFit/18000/theta_diff
 
