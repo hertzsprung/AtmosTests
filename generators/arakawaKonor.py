5fcb68b4e76dac78bb5905dbb7ac18c910c447de
James Shaw <js102@zepler.net>
2017-10-06 16:59:31 +0100

    add fast mesh for arakawaKonor

--- generators/arakawaKonor.py
+++ generators/arakawaKonor.py
@@ -8,9 +8,11 @@ class ArakawaKonor:
         lorenz = ninja.ArakawaKonor.lorenz
         linearUpwind = os.path.join('src/schaerWaves/linearUpwind')
 
+        self.meshFast = BlockMesh('arakawaKonor-mesh-fast', os.path.join('src/arakawaKonor/mesh-fast'))
         self.meshUniform = BlockMesh('arakawaKonor-mesh-uniform', os.path.join('src/arakawaKonor/mesh-uniform'))
-        self.uniform = ninja.ArakawaKonor('arakawaKonor-uniform', self.meshUniform, lorenz, linearUpwind, parallel, fast)
+        self.uniform = ninja.ArakawaKonor('arakawaKonor-uniform', self.meshUniform, lorenz, linearUpwind, parallel, fast, self.meshFast)
 
     def addTo(self, build):
         build.add(self.meshUniform)
+        build.add(self.meshFast)
         build.add(self.uniform)
