da1ffcd2104b7fdc27b3f5f40bdf2afe9359191a
James Shaw <js102@zepler.net>
2017-10-09 14:59:05 +0100

    add more schaerWaves linearUpwind tests

--- generators/schaerWaves.py
+++ generators/schaerWaves.py
@@ -9,12 +9,17 @@ class SchaerWaves:
         lorenz = ninja.SchaerWaves.lorenz
 
         self.btfLinearUpwind300dz = ninja.SchaerWaves('schaerWaves-btf-300dz-linearUpwind', meshes.btf300dz, 8.0, lorenz, linearUpwind, parallel, fast, meshes.btfFast)
+        self.cutCellLinearUpwind300dz = ninja.SchaerWaves('schaerWaves-cutCell-300dz-linearUpwind', meshes.cutCell300dz, 8.0, lorenz, linearUpwind, parallel, fast, meshes.btfFast)
+        self.slantedCellLinearUpwind300dz = ninja.SchaerWaves('schaerWaves-slantedCell-300dz-linearUpwind', meshes.slantedCell300dz, 8.0, lorenz, linearUpwind, parallel, fast, meshes.btfFast)
+
         self.btfCubicFit300dz = ninja.SchaerWaves('schaerWaves-btf-300dz-cubicFit', meshes.btf300dz, 8.0, lorenz, cubicFit, parallel, fast, meshes.btfFast)
         self.cutCellCubicFit300dz = ninja.SchaerWaves('schaerWaves-cutCell-300dz-cubicFit', meshes.cutCell300dz, 8.0, lorenz, cubicFit, parallel, fast, meshes.btfFast)
         self.slantedCellCubicFit300dz = ninja.SchaerWaves('schaerWaves-slantedCell-300dz-cubicFit', meshes.slantedCell300dz, 8.0, lorenz, cubicFit, parallel, fast, meshes.btfFast)
 
     def addTo(self, build):
         build.add(self.btfLinearUpwind300dz)
+        build.add(self.cutCellLinearUpwind300dz)
+        build.add(self.slantedCellLinearUpwind300dz)
         build.add(self.btfCubicFit300dz)
         build.add(self.cutCellCubicFit300dz)
         build.add(self.slantedCellCubicFit300dz)
