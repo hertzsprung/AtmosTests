54e7cb036496b768e228981ff0d329c594524722
James Shaw <js102@zepler.net>
2017-11-23 17:15:53 +0000

    add arakawaKonorAdvect horizontalGrading tests

--- generators/arakawaKonorAdvect.py
+++ generators/arakawaKonorAdvect.py
@@ -12,6 +12,11 @@ class ArakawaKonorAdvect:
         self.uniformLorenz = ninja.ArakawaKonorAdvect('arakawaKonorAdvect-uniform-lorenz', meshes.uniform, lorenz, linearUpwind, parallel, fast, meshes.fast)
         self.uniformCP = ninja.ArakawaKonorAdvect('arakawaKonorAdvect-uniform-cp', meshes.uniform, cp, cpFvSchemes, parallel, fast, meshes.fast)
 
+        self.horizontalGradingLorenz = ninja.ArakawaKonorAdvect('arakawaKonorAdvect-horizontalGrading-lorenz', meshes.horizontalGrading, lorenz, linearUpwind, parallel, fast, meshes.fast)
+        self.horizontalGradingCP = ninja.ArakawaKonorAdvect('arakawaKonorAdvect-horizontalGrading-cp', meshes.horizontalGrading, cp, cpFvSchemes, parallel, fast, meshes.fast)
+
     def addTo(self, build):
         build.add(self.uniformLorenz)
         build.add(self.uniformCP)
+        build.add(self.horizontalGradingLorenz)
+        build.add(self.horizontalGradingCP)
