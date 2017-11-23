9985ac1dc227eac042cc336e104a6586ee1b91b9
James Shaw <js102@zepler.net>
2017-11-23 16:33:35 +0000

    create arakawaKonorAdvect charneyPhillips test

--- generators/arakawaKonorAdvect.py
+++ generators/arakawaKonorAdvect.py
@@ -7,8 +7,11 @@ class ArakawaKonorAdvect:
         lorenz = ninja.ArakawaKonor.lorenz
         cp = ninja.ArakawaKonor.charneyPhillips
         linearUpwind = os.path.join('src/schaerAdvect/linearUpwind')
+        cpFvSchemes = os.path.join('src/arakawaKonorAdvect/charneyPhillipsFvSchemes')
 
         self.uniformLorenz = ninja.ArakawaKonorAdvect('arakawaKonorAdvect-uniform-lorenz', meshes.uniform, lorenz, linearUpwind, parallel, fast, meshes.fast)
+        self.uniformCP = ninja.ArakawaKonorAdvect('arakawaKonorAdvect-uniform-cp', meshes.uniform, cp, cpFvSchemes, parallel, fast, meshes.fast)
 
     def addTo(self, build):
         build.add(self.uniformLorenz)
+        build.add(self.uniformCP)
