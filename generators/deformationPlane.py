6b4f452fc796fcfbeb637130b01239e959ed8993
James Shaw <js102@zepler.net>
2018-02-05 13:40:05 +0000

    add deformationPlane highOrderFit

--- generators/deformationPlane.py
+++ generators/deformationPlane.py
@@ -17,11 +17,22 @@ class DeformationPlane:
                 'deformationPlane-uniform-cubicFit-collated',
                 fvSchemes=os.path.join('src/deformationPlane/cubicFit'),
                 tests=[
-                    DeformationPlaneCollated.Test('deformationPlane-uniform-3-cubicFit', 3, meshUniform3, timestep=0.004)
+                    DeformationPlaneCollated.Test('deformationPlane-uniform-3-cubicFit', 3, meshUniform3, timestep=0.005)
                 ])
+
+        self.uniformHighOrderFitCollated = deformationPlane.collated(
+                'deformationPlane-uniform-highOrderFit-collated',
+                fvSchemes=os.path.join('src/deformationPlane/highOrderFit'),
+                tests=[
+                    DeformationPlaneCollated.Test('deformationPlane-uniform-3-highOrderFit', 3, meshUniform3, timestep=0.005)
+                ],
+                controlDict=os.path.join('src/deformationPlane/controlDict.highOrderFit.template'),
+                solverRule='scalarDeformationHighOrderFit')
 
     def addTo(self, build):
         build.addAll(self.meshes)
 
         build.add(self.uniformCubicFitCollated)
         build.addAll(self.uniformCubicFitCollated.tests)
+        build.add(self.uniformHighOrderFitCollated)
+        build.addAll(self.uniformHighOrderFitCollated.tests)
